52d5808c3f67cd3b93880a7888352758bd1b62ae
prateek <prateekangel8127@gmail.com>
2023-09-05 13:32:18 +0530

    Adding all files

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thing all doing well</title>
+    <title>Building Wordpress Theme.</title>
 </head>
 <body>
-    
+    <h1>I am Prateek Angel</h1>
 </body>
 </html>
