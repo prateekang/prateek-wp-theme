5a55659cad92b765c06909c757eaa420dbe34055
prateek <prateekangel8127@gmail.com>
2023-09-06 16:46:11 +0530

    adding all files

--- functions.php
+++ functions.php
@@ -2,9 +2,11 @@
 
 add_action('wp_enqueue_scripts',function(){
  wp_enqueue_style('theme-css', get_stylesheet_directory_uri().'/css/main.css', array(), time() );
-
-//  wp_enqueue_style('navbar-css', get_stylesheet_directory_uri().'/css/navbar.css', array(), time() );
-
 });
+//  wp_enqueue_style('navbar-css', get_stylesheet_directory_uri().'/css/navbar.css', array(), time() );
+add_action('after_setup_theme','theme_setup');
 
-// add_theme_support("post-thumbnail");
+function theme_setup(){
+ /* post thumbnail */
+    add_theme_support( 'post-thumbnails' );
+}
