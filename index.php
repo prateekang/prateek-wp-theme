251bf9038167619153e240ab3a9a2a8225face21
prateek <prateekangel8127@gmail.com>
2023-09-06 11:31:36 +0530

    adding all fileS

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<h1>This is my first index php code</h1>
-
-<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ChurchBuzz</title>
-</head>
-<body>
-    <h1>The Man Who Climbed The Tree</p>
-    <p>Zacchaeus encounters Jesus and his life turns around. He gives up his belongings to the poor and repays to those he swindled. How did this all happen?
-
-</p>
-</body>
-</html>
+<?php get_header(); ?>
+<?php 
+while(have_posts()):
+    the_post();
+    the_title();
+endwhile;
+?>
