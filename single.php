bafb23c782badec2b87436d6bf1a3fed2c7f1927
prateek <prateekangel8127@gmail.com>
2023-09-07 10:00:00 +0530

    Adding all files

--- single.php
+++ single.php
@@ -5,7 +5,7 @@ while(have_posts()):
     the_post();
     the_title();
 get_template_part('template/content/content-single');
-
+the_post_thumbnail(array(100,100));    
 endwhile;
 ?>
 <?php get_footer(); ?>
