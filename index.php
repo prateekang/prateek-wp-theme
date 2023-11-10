54b8517705aa945d6b3a5ea16168e4ee2b191962
Prateek Angel <prateekangel8127@gmail.com>
2023-11-10 04:25:35 -0500

    adding all files

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<h1>hello world</h1>
+<?php get_header(); ?>
+
+<?php
+while(have_posts()):
+    the_post();
+    the_title();
+get_template_part('template/content/content-single');
+the_post_thumbnail(); 
+the_excerpt();    
+the_author();
+the_author_meta('description') ;
+the_author_meta('avator');
+the_category();
+the_tags();
+the_post_navigation();
+previous_post_link();
+next_post_link();
+endwhile;
+?>
+<?php get_footer(); ?>
